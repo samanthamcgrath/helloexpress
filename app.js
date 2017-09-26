5be13890746afe80c6de067bdfa6b49a56535411
Samantha McGrath <sam.a.mcgrath@gmail.com>
2017-09-26 16:40:40 +1000

    Added random number generator which uses max number of comics from xkcd homepage. Scrapes relevant page to find the image url of this randomly chosen comic. Serves the comic.

--- app.js
+++ app.js
@@ -1,10 +1,51 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const https = require('https');
+const app = express();
+
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+    https.get('https://xkcd.com', function(result) {
+        //console.log("Got response: " + result.statusCode);
+        //console.log('HEADERS: ' + JSON.stringify(result.headers));
+        result.setEncoding('utf8');
+        let rawData = '';
+        result.on('data', (chunk) => { 
+            rawData += chunk; 
+        });
+        result.on('end', () => {
+            var re = new RegExp('Permanent link to this comic: https://xkcd.com/(\\d+)/');
+            var num = re.exec(rawData);
+            var rand = Math.floor((Math.random() * num[1]) + 1);
+            console.log('num = ' + num[1]);
+            console.log('rand = ' + rand);
+            var link = 'https://xkcd.com/' + rand + '/';
+            console.log(link);
+            https.get(link, function(result2) {
+                result2.setEncoding('utf8');
+                console.log("Got response: " + result2.statusCode);
+
+                let myData = '';
+                result2.on('data', (chunk2) => { 
+                    //console.log(chunk2);
+                    myData += chunk2; 
+                });
+                result2.on('end', () => {
+                    //console.log(myData);
+                    var reg = new RegExp('(https://imgs.xkcd.com/comics/.*.png)', 'i');
+                    var imgSrc = reg.exec(myData);
+                    console.log("img src = " + imgSrc[1]);
+                    var html = "<img src=\"" + imgSrc[1] + "\">";
+                    res.send(html);
+                    res.end();
+                });
+            });
+
+            
+        });
+    });
+    
+});
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('Example app listening on port 3000!');
+});
